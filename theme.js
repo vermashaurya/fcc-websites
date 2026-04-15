b9dda4640c859c13c4cf5fccd7d565efc1ca3c83
vermashaurya <shauryathegreatest@gmail.com>
2026-04-15 21:06:37 +0530

    revert

--- theme.js
+++
@@ -1,8 +0,0 @@
-const toggle = document.getElementById("themeToggle");
-
-toggle.addEventListener("click", () => {
-  document.body.classList.toggle("light");
-
-  toggle.textContent =
-    document.body.classList.contains("light") ? "🌙" : "☀️";
-});
